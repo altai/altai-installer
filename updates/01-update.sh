236fc8025f106375944457007f5a7a803297e683
Alessio Ababilov <aababilov@griddynamics.com>
2012-09-26 16:51:09 +0300

    save altai-release to altai-install-info.json during update
    Change-Id: I92a00ab53b7375ed9577b42744db549245aab6fd

--- updates/01-update.sh
+++ updates/01-update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 function is_master() {
-    rpm -q python-focus
+    rpm -q python-focus &>/dev/null
 }
 
 master_services="focus instance-notifier zabbix-notifier nova-billing-heart nova-billing-os-amqp"
 master_packages="python-focus instance-notifier nova-fping-ext python-openstackclient-base-essex zabbix-notifier"
+
+mv /etc/altai-release /etc/altai-install-info.json
 
 if is_master; then
     for srv in $master_services; do
@@ -16,7 +18,7 @@ if is_master; then
         service $srv start
     done
 else
-    compute_ip_private=$(python -c 'import json; print json.load(open("/opt/altai/compute-node.json"))["compute-ip-private"]')
+    compute_ip_private=$(python -c 'import json; print json.load(open("/etc/altai-install-info.json"))["compute-ip-private"]')
     sed -i "s/vncserver_proxyclient_address.*/vncserver_proxyclient_address = $compute_ip_private/" /etc/nova/nova.conf
     service nova-compute restart
 fi
