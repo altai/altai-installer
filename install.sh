b164d57257aad94d8be3c06bb3d953ce0f61d1b6
Alessio Ababilov <aababilov@griddynamics.com>
2012-12-27 11:31:19 +0200

    Add selinux status verification to installation
    Issue: altai-67
    Change-Id: I741b138a8d9602df1e71c1e6e5e80348baf38e32

--- install.sh
+++ install.sh
@@ -40,12 +40,33 @@ then
     usage
 fi
 
+# set Permissive mode if SELinux is enabled
+if selinuxenabled; then
+    setenforce 0
+fi
+
+# install packages
 receipt="${1}-node.json"
 altailog="/var/log/altai-install.log"
 touch "$altailog"
 chmod 600 "$altailog"
 ./_install.sh "$DIR" "$1" "$receipt" "$altailog" 2>&1 | tee -a "$altailog"
 
+# disable SELinux
+if selinuxenabled; then
+    sed --follow-symlinks -i 's/SELINUX=.*$/SELINUX=disabled/' /etc/sysconfig/selinux
+    echo -e "\033[47m\033[1;31m**************************************************\033[0m"
+    echo -e "\033[47m\033[1;31m*\033[0m \033[40m\033[1;31m                                               \033[47m\033[1;31m*\033[0m"
+    echo -e "\033[47m\033[1;31m*\033[0m \033[40m\033[1;31m>> \033[5mYou had SELinux enabled on your host!  \033[25m <<  \033[47m\033[1;31m*\033[0m"
+    echo -e "\033[47m\033[1;31m*\033[0m \033[40m\033[1;31mAltai cannot work if SELinux is enabled.       \033[47m\033[1;31m*\033[0m"
+    echo -e "\033[47m\033[1;31m*\033[0m \033[40m\033[1;31m                                               \033[47m\033[1;31m*\033[0m"
+    echo -e "\033[47m\033[1;31m*\033[0m \033[40m\033[1;31mSELinux is now switched to Permissive mode     \033[47m\033[1;31m*\033[0m"
+    echo -e "\033[47m\033[1;31m*\033[0m \033[40m\033[1;31mand disabled in /etc/sysconfig/selinux!        \033[47m\033[1;31m*\033[0m"
+    echo -e "\033[47m\033[1;31m*\033[0m \033[40m\033[1;31m                                               \033[47m\033[1;31m*\033[0m"
+    echo -e "\033[47m\033[1;31m**************************************************\033[0m"
+fi
+
+# save version and release
 altai_release_file=/etc/altai-release
 rpm -q --queryformat '%{VERSION}\n' altai-release > $altai_release_file
 
