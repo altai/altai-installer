70a8977efb21257519a97e0142bbde041a2fd058
Sergey Chinilov <schinilov@griddynamics.com>
2013-02-07 14:01:23 +0400

    NTP force sync setup
    Change-Id: Ie42653a9674938725cbf0aeedec3d067fb2dc5b7

--- cookbooks/devgrid/recipes/compute.rb
+++ cookbooks/devgrid/recipes/compute.rb
@@ -18,7 +18,7 @@ require "rubygems"
 
 log("Start to install nova-compute")
 
-%w( ntp dbus openstack-nova-compute ).each do |package_name|
+%w( dbus openstack-nova-compute ).each do |package_name|
     package package_name 
 end
 
@@ -55,17 +55,9 @@ end
 
 
 node["services"].push({"name"=>"nova_compute", "type"=>"amqp"})
-%w(ntpd messagebus libvirtd iptables nova-compute).each do |service|
+%w( messagebus libvirtd nova-compute ).each do |service|
     service service do
 	action [:enable, :restart]
-    end
-end
-
-node["services"].push({"name"=>"nova_compute", "type"=>"amqp"})
-%w(nova-network).each do |service|
-    service service do
-        action [:enable, :restart]
-        only_if "test $ROLE = master"
     end
 end
 
