bdd04ee295b9dfa47fd39ccddaa4c96aa54ce22e
Ivan A. Melnikov <imelnikov@griddynamics.com>
2013-04-17 19:17:48 +0400

    Drop nametype_index for nova_dns when updating to v1.1.3
    See nova-dns change I3f7f4736ceb6cdb0a7a0241e99db45f3c57da7d8.
    
    Change-Id: If1987e6a21b3f7ec614b4280f0da742dceb2220e

--- updates/update-1.1.3.sh
+++ updates/update-1.1.3.sh
@@ -21,5 +21,11 @@ nova-objectstore
 nova-scheduler
 "
 
+function postin_master() {
+    # NOTE(imelnikov): see nova-dns change I3f7f4736ceb6cdb0a7a0241e99db45f3c57da7d8
+    mysql_root_password=$(python -c 'import json; print json.load(open("/opt/altai/altai-node.json"))["mysql-root-password"]')
+    mysql -u root --password="$mysql_root_password" dns -e 'DROP INDEX nametype_index ON records;' || true
+}
+
 source "$(dirname "$0")/functions.sh"
 standard_update "$@"
